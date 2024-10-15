ad11eed8c1914bd91d4a74f9fc1d48b1a8d82528
Svitla-Dan <svetakorj11@gmail.com>
2024-10-15 19:38:19 +0200

    Fix to Homework

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -2,11 +2,12 @@ import style from "./ImageCard.module.css";
 
 const ImageCard = ({ image, onClick }) => {
   return (
-    <div className={style.card} onClick={() => onClick(image)}>
+    <div className={style.card}>
       <img
         className={style.image}
         src={image.urls.small}
         alt={image.alt_description}
+        onClick={() => onClick(image)}
       />
       <ul className={style.info}>
         <li className={style.author}> {image.user.name}</li>
